7f944eccbfad8dd716b05aba8bb21e694db6a8b8
Alberto <aldo10012@gmail.com>
2022-03-04 16:43:36 -0800

    fix like state buttons

--- src/components/Like/Like.js
+++ src/components/Like/Like.js
@@ -3,30 +3,21 @@ import react, { useState } from 'react'
 
 
 function Like() {
-    const [num, setNum] = useState(0)
+    const [count, setCount] = useState(0)
 
     const IncrementItem = () => {
-        setNum(Number(num) + 1);
+        setCount(count + 1);
     }
     
     const DecreaseItem = () => {
-        setNum(Number(num) - 1);
+        setCount(count - 1);
     }
-
-    let handleChange = (e)=>{
-        setNum(e.target.value);
-    }
-
-
 
     return(
         <div className='Like'>
-            <button type="button" onChange={DecreaseItem}>-1</button>
-
-            <h2 value={num}>{num}</h2>
-            <input type="text" className="form-control" value={num} onChange={handleChange}/>
-
-            <button type="button" onChange={IncrementItem}>+1</button>
+            <button onClick={DecreaseItem}>-1</button>
+            <h2 value={count}>{count}</h2>
+            <button type="button" onClick={IncrementItem}>+1</button>
         </div>
     )
 }
